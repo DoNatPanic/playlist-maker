7ca57daf25e5b8507c31d3dbf5b8fa211d07419b
Faina Shi <shigapovafaina@gmail.com>
2024-04-10 13:59:26 +0300

    Sprint 8. Homework

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchCard = findViewById<FrameLayout>(R.id.search_card)
+        val mediaCard = findViewById<FrameLayout>(R.id.media_card)
+        val settingsCard = findViewById<FrameLayout>(R.id.settings_card)
+
+        // переход на экран поиска
+        searchCard.setOnClickListener {
+            val displayIntent = Intent(this, SeachActivity::class.java)
+            startActivity(displayIntent)
         }
+
+        // экран медиатеки
+        mediaCard.setOnClickListener {
+            val displayIntent = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        // экран настроек
+        settingsCard.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+//        // 1-ый способ
+//        val cardClickListener: View.OnClickListener = object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+//                Toast.makeText(this@MainActivity, "Поиск композиции", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//        searchCard.setOnClickListener(cardClickListener)
+//
+//        // 2-ой способ через лямбда-выражение
+//        searchCard.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "Поиск композиции", Toast.LENGTH_SHORT).show()
+//        }
     }
 }
