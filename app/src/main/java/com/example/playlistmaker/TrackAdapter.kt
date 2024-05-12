4deeb8e9146588575e2d725aa7c62254c671e25a
Faina Shi <shigapovafaina@gmail.com>
2024-05-12 16:57:36 +0300

    Sprint 12. Tracks history

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -5,9 +5,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TrackAdapter(
-    private val tracks: List<Track>
-) : RecyclerView.Adapter<TrackViewHolder> () {
-
+    var tracks: List<Track>,
+    private val searchActivity: SearchActivity?
+) : RecyclerView.Adapter<TrackViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
@@ -16,6 +16,9 @@ class TrackAdapter(
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position])
+        holder.itemView.setOnClickListener {
+            searchActivity?.rebuildTrackHistoryList(position)
+        }
     }
 
     override fun getItemCount(): Int {
